59fe214e4a10332bb185a02831308625a788299f
3m1n3nc3 <mygames.ng@gmail.com>
2024-08-02 11:36:24 +0100

    Add relevant default messages

--- config/messages.php
+++ config/messages.php
@@ -9,7 +9,7 @@ return [
     | This is required and will be attached to the end of every message
     |
     */
-    'signature' => "Isah Raphael<br />CEO<br />Greysoft Technologies Limited",
+    'signature' => 'Regards,<br />',
 
     /*
     |--------------------------------------------------------------------------
@@ -23,16 +23,96 @@ return [
     | the color property is optional.
     |
     */
-    'new_message' => [
-        "subject" => "New Message Received from :username",
+
+    /*
+    |--------------------------------------------------------------------------
+    | SendCode messages
+    |--------------------------------------------------------------------------
+    |
+    | These are messages sent during account or request verification.
+    |
+    */
+    'send_code::reset' => [
+        "subject" => "Reset your :app_name password.",
         "lines" => [
-            ":fullname sent you a message.",
+            "Hello :firstname,",
+            "You are receiving this email because we received a password reset request for your account on Niconpay.",
+            "Use the code or link below to recover your account.",
+            "<h3 style=\"text-align: center;\">:code</h3>",
             [
-                'link' => 'https://example.com/read',
-                'title' => 'Read It',
-                'color' => '#fff'
+                'link' => 'https://niconpay.com/reset/password?token=:token',
+                'title' => 'Reset Password'
             ],
-            "You can choose to read it or ignore it.",
+            "This password reset code will expire in :duration.",
+            "If you did not request a password reset, no further action is required.",
+        ],
+    ],
+    'send_code::verify' => [
+        "subject" => "Verify your account on :app_name.",
+        "lines" => [
+            "Hello :firstname,",
+            "You are receiving this email because you created an account on <b>:app_name</b> and we needed to verify that you own this email addrress. <br />Use the code or link below to verify your email address.",
+            "<h3 style=\"text-align: center;\">:code</h3>",
+            [
+                'link' => "https://niconpay.com/account/verify/:type?token=:token",
+                'title' => 'Verify Account'
+            ],
+            "This verification code will expire in :duration.",
+            "If you do not recognize this activity, no further action is required as the associated account will be deleted in few days if left unverified.",
+        ],
+    ],
+    'send_code::otp' => [
+        "subject" => "Your One Time Password.",
+        "lines" => [
+            "Use the code below to verify your request.",
+            "<h3 style=\"text-align: center;\">:code</h3>",
+            "This OTP will expire in :duration.",
+            "If you do not recognize this request no further action is required or you can take steps to secure your account.",
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | SendVerified messages
+    |--------------------------------------------------------------------------
+    |
+    | These are messages sent after an account is verified.
+    |
+    */
+    'send_verified' => [
+        "subject" => "Welcome to the Niconpay community.",
+        "lines" => [
+            "Hello :firstname,",
+            "Your :app_name account has been verified sucessfully and we want to use this opportunity to welcome you to our community.",
+            [
+                'link' => "https://niconpay.com/login",
+                'title' => 'Get Started'
+            ],
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Other messages
+    |--------------------------------------------------------------------------
+    |
+    |
+    */
+    'email_verification' => [
+        'subject' => 'Please verify your email.',
+        'lines' => [
+            'Hello :fullname,',
+            'You initiated an account opening proccess at :app_name, please use the code below to complete your request',
+            '<h3 style="text-align: center;">:code</h3>',
+            'If you need any further assistance please reachout to support.',
+        ],
+    ],
+    'welcome' => [
+        'subject' => ':firstname, welcome to :app_name.',
+        'lines' => [
+            'Hello :fullname,',
+            "We are happy to have you onboard :app_name, your registration was successfull and we can't wait to see what you do next.",
+            "If you do need any assistance please don't fail to reachout to one of our numerous support channels.",
         ],
     ],
 ];
