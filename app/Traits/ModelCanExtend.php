57d9d8f1ad244336c68fc2cd1a63d831ce91497c
3m1n3nc3 <mygames.ng@gmail.com>
2024-09-19 18:38:42 +0100

    Feat: Add $separator and $additional params to generateUsername ModelCanExtend trait method.

--- app/Traits/ModelCanExtend.php
+++ app/Traits/ModelCanExtend.php
@@ -9,15 +9,30 @@ trait ModelCanExtend
     /**
      * Generate a Username for the user.
      *
-     * @param  \Illuminate\Support\Stringable|string  $string
+     * @param string $string
+     * @param string $field
+     * @param string $separator
+     * @param array{class-string, string} $additional
      * @return string
      */
-    protected static function generateUsername($string, $field = 'username')
-    {
-        $username = str($string)->slug('_');
+    protected static function generateUsername(
+        string $string,
+        string $field = 'username',
+        string $separator = '_',
+        array $additional = []
+    ) {
+        $username = str($string)->slug($separator);
 
         if (static::where($field, $username)->exists()) {
-            $username = $username->append('-')->append(Random::number(1111, 9999));
+            $username = $username->append($separator)->append(Random::number(1111, 9999));
+        }
+
+        if (
+            isset($additional[0]) &&
+            method_exists($additional[0], 'where') &&
+            static::where($additional[0], $additional[1] ?? $field)->exists()
+        ) {
+            $username = $username->append($separator)->append(Random::number(1111, 9999));
         }
 
         return $username->toString();
