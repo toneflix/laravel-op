b594fb203716120f98853fab1e21492be2517015
3m1n3nc3 <mygames.ng@gmail.com>
2025-01-25 11:44:11 +0100

    fix: Define  $responseKeys in Providers.

--- app/Helpers/Providers.php
+++ app/Helpers/Providers.php
@@ -23,6 +23,8 @@ use ToneflixCode\DbConfig\Helpers\Configure;
 
 class Providers
 {
+    protected static array $responseKeys = [];
+
     /**
      * Get / set the specified configuration value.
      *
@@ -188,12 +190,12 @@ class Providers
     public static function paginator(LengthAwarePaginator $data): array
     {
         if ($data instanceof LengthAwarePaginator) {
-            $links = $data->linkCollection()->filter(fn ($link) => is_numeric($link['label']));
+            $links = $data->linkCollection()->filter(fn($link) => is_numeric($link['label']));
 
             return [
                 'data' => count(static::$responseKeys)
                     ? collect($data->items())
-                        ->map(fn ($e) => collect($e)->filter(fn ($k, $v) => in_array($v, static::$responseKeys)))
+                    ->map(fn($e) => collect($e)->filter(fn($k, $v) => in_array($v, static::$responseKeys)))
                     : $data->items(),
                 'meta' => [
                     'current_page' => $data->currentPage(),
@@ -242,7 +244,7 @@ class Providers
      */
     public static function money($number, $abbrev = false)
     {
-        return static::config('currency_symbol').(
+        return static::config('currency_symbol') . (
             $abbrev === false
             ? number_format($number, 2)
             : static::numberAbbr($number)
@@ -286,11 +288,11 @@ class Providers
         // Remove unecessary zeroes after decimal. "1.0" -> "1"; "1.00" -> "1"
         // Intentionally does not affect partials, eg "1.50" -> "1.50"
         if ($precision > 0) {
-            $dotzero = '.'.str_repeat('0', $precision);
+            $dotzero = '.' . str_repeat('0', $precision);
             $n_format = str_replace($dotzero, '', $n_format);
         }
 
-        return $n_format.$suffix;
+        return $n_format . $suffix;
     }
 
     /**
